53a4f559f3115b0bcfe14092f49b234676610110
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-10-21 13:59:10 +0300

    Added UTC now

--- 2024_daler_khomidov_tg_bot_001.py
+++ 2024_daler_khomidov_tg_bot_001.py
@@ -4,51 +4,40 @@ from airflow.models import Variable
 from datetime import datetime, timedelta
 import requests
 
-# Default arguments for the DAG
 default_args = {
     'owner': 'кошкодевочка',
-    'start_date': datetime(2024, 7, 19, 22, 40),  # Установите желаемую дату и время начала
+    'start_date': datetime(2024, 7, 19, 22, 40), 
     'retries': 2,
     'retry_delay': timedelta(minutes=5),
 }
 
-# Функция для отправки сообщения в Telegram
 def send_airflow_alive_message():
-    # Получение переменных Airflow
     bot_token = "8128045612:AAENaeVPYa9YHCzJfk07aYCSmOLMEplaqAM" #Variable.get('telegram_bot_token')
     chat_id = "-1002095886585" #Variable.get('telegram_chat_id')
-
-    # Формирование сообщения
-    message = "🐱 *Airflow жив!* 🐱"
-
-    # URL для отправки сообщения через Telegram API
+    utc_now = datetime.now()
+    utc_plus_3 = utc_now + timedelta(hours=3)
+    formatted_time = utc_plus_3.strftime("%Y-%m-%d %H:%M:%S")
+    message = f"🐱 *Airflow жив!* 🐱\nОтчетное время (UTC+3): {formatted_time}"
+    return message
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-
-    # Параметры запроса
     payload = {
         "chat_id": chat_id,
         "text": message,
         "parse_mode": "Markdown"
     }
-       # URL для отправки сообщения
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-
-    # Параметры запроса
     payload = {
         "chat_id": chat_id,
         "text": message,
         "parse_mode": "Markdown"
     }
-    message_thread_id = 15206#Variable.get('telegram_message_thread_id', default_var=None)
-
-    # Добавление message_thread_id, если он задан
+    message_thread_id = 15206 #Variable.get('telegram_message_thread_id', default_var=None)
     if message_thread_id:
         payload["message_thread_id"] = message_thread_id
 
     try:
-        # Отправка POST-запроса
         response = requests.post(url, data=payload)
-        response.raise_for_status()  # Проверка на успешный статус
+        response.raise_for_status()  
         print(f"Сообщение успешно отправлено: {response.json()}")
     except requests.exceptions.HTTPError as errh:
         print(f"HTTP ошибка: {errh} - Ответ: {response.text}")
@@ -67,7 +56,7 @@ with DAG(
     dag_id='simple_airflow_alive_report',
     default_args=default_args,
     description='Простой DAG для отправки сообщения "Airflow жив" в Telegram',
-    schedule_interval=timedelta(hours=1),  # Запуск раз в час
+    schedule_interval=timedelta(hours=1),  
     catchup=False,
     tags=['simple', 'telegram', 'health-check'],
 ) as dag:
